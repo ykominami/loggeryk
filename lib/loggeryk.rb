ece629c171fd56b6974f7627d597f17b1a71305b
yasuo kominami <ykominami@gmail.com>
2021-05-17 11:00:19 +0900

    add gems

---
+++ lib/loggeryk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "loggeryk/version"
+
+module Loggeryk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/loggeryk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Loggeryk do
+  it "has a version number" do
+    expect(Loggeryk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
